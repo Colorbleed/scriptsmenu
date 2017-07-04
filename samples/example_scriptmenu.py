698f2e875267a001f14cf959eccd476a2d97b59d
aardschok <wikoreman@gmail.com>
2017-07-04 15:17:39 +0200

    cosmetics

--- samples/example_scriptmenu.py
+++ samples/example_scriptmenu.py
@@ -5,14 +5,14 @@ from Qt import QtWidgets, QtGui
 
 from scriptsmenu import ScriptsMenu
 from scriptsmenu.scriptsmenu import (
-    _load_configuration,
+    load_configuration,
     load_from_configuration
 )
 
 # set the example evironment variable
 os.environ["SCRIPTMENU"] = os.path.dirname(__file__)
 config = os.path.expandvars(r"$SCRIPTMENU/sample_configuration_a.json")
-config = _load_configuration(config)    # parse the .json file
+config = load_configuration(config)    # parse the .json file
 
 app = QtWidgets.QApplication(sys.argv)
 
